09d3f28f0c191a6680e61b9f9a893ebb2027eb09
pesenteur <931373882@qq.com>
2023-08-24 16:16:28 +0800

    init2

--- src/main.js
+++ src/main.js
@@ -1,15 +1,4 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import 'dayjs/locale/zh-cn'
-import api from "@/api";
-import 'element-plus/dist/index.css'
-import {createPinia} from "pinia";
 
-const app = createApp(App);
-
-app.use(router);
-app.use(createPinia());
-app.use(api);
-
-app.mount('#app');
+createApp(App).mount('#app')

5711390511f7b71202185ac89d86cf8e4dd859c2
Thebearcoding <1193617419@qq.com>
2023-08-25 01:41:30 +0800

    更新导航栏

--- src/home/HomeCarousel.vue
+++ src/home/HomeCarousel.vue
@@ -1,35 +1,20 @@
 <script setup>
-  import {reactive} from "vue";
+import { reactive } from "vue";
 
 </script>
 
 
 <template>
   <el-carousel indicator-position="outside" class="main">
-    <el-carousel-item v-for="item in 4" :key="item" class="main" >
-      <img src="../assets/cat1.png">
+    <el-carousel-item v-for="item in 4" :key="item" class="main">
+      <img src="../assets/cat1.png" alt="" style="width:100%;height: 100%;">
     </el-carousel-item>
   </el-carousel>
 </template>
 
 <style scoped>
-.main{
+.main {
   width: 730px;
   height: 400px;
-}
-.el-carousel__item h3 {
-  display: flex;
-  color: #475669;
-  opacity: 0.75;
-  line-height: 400px;
-  margin: 0;
-}
-
-.el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
-}
-
-.el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
 }
 </style>

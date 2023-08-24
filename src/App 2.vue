1a2a787424f4925aee3bf2699229b9193939ba7b
pesenteur <931373882@qq.com>
2023-08-24 22:47:06 +0800

    canceal

---
+++ src/App 2.vue
@@ -0,0 +1,28 @@
+<template>
+  <router-view/>
+</template>
+
+<script setup>
+
+import TeamFooter from './components/TeamFooter.vue'
+import TeamHeader from './components/TeamHeader.vue'
+
+//
+// export default {
+//   name: 'App',
+//   components: {
+//     TeamFooter
+//   }
+// }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/account/SearchBar 2.vue
@@ -0,0 +1,14 @@
+<template>
+    <form class="search-bar">
+        <input type="text" placeholder="Search..." />
+        <button><i class='bx bx-search'></i></button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar'
+};
+</script>
+
+<style scoped></style>

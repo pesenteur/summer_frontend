77d8c2a3ade166578632243989785df630505fa2
pesenteur <931373882@qq.com>
2023-08-25 16:44:33 +0800

    update

--- src/views/myTest/myTest.vue
+++ src/views/myTest/myTest.vue
@@ -1,11 +1,48 @@
 <template>
-  <h2>1111111</h2>
+  <div class="flex flex-wrap items-center">
+    <el-dropdown>
+      <el-button text class="custom-icon-button">
+        <el-icon class="el-icon--right"><arrow-down /></el-icon>
+      </el-button>
+      <template #dropdown>
+        <el-dropdown-menu>
+          <el-dropdown-item>Action 1</el-dropdown-item>
+          <el-dropdown-item>Action 2</el-dropdown-item>
+          <el-dropdown-item>Action 3</el-dropdown-item>
+          <el-dropdown-item>Action 4</el-dropdown-item>
+          <el-dropdown-item>Action 5</el-dropdown-item>
+        </el-dropdown-menu>
+      </template>
+    </el-dropdown>
+
+  </div>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { ArrowDown } from '@element-plus/icons-vue'
 
+const handleClick = () => {
+  // eslint-disable-next-line no-alert
+  alert('button click')
+}
 </script>
+<style scoped>
+.example-showcase .el-dropdown + .el-dropdown {
+  margin-left: 15px;
+}
+.example-showcase .el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
+}
 
-<style>
+.custom-icon-button {
+  background-image: url('@/assets/avtar.png'); /* 根据您的项目路径调整 */
+  background-size: 40px; /* 根据需要调整 */
+  background-repeat:no-repeat;
+  width: 40px; /* 根据需要调整 */
+  height: 40px; /* 根据需要调整 */
 
+}
 </style>

eede264b14e9b1d1206cf98f0f2970b5fc328d00
pesenteur <931373882@qq.com>
2023-08-26 00:18:53 +0800

    update

--- src/views/myTest/myTest.vue
+++ src/views/myTest/myTest.vue
@@ -1,48 +1,57 @@
 <template>
-  <div class="flex flex-wrap items-center">
-    <el-dropdown>
-      <el-button text class="custom-icon-button">
-        <el-icon class="el-icon--right"><arrow-down /></el-icon>
-      </el-button>
-      <template #dropdown>
-        <el-dropdown-menu>
-          <el-dropdown-item>Action 1</el-dropdown-item>
-          <el-dropdown-item>Action 2</el-dropdown-item>
-          <el-dropdown-item>Action 3</el-dropdown-item>
-          <el-dropdown-item>Action 4</el-dropdown-item>
-          <el-dropdown-item>Action 5</el-dropdown-item>
-        </el-dropdown-menu>
-      </template>
-    </el-dropdown>
-
+  <!-- Form -->
+  <div>
+  <el-button text @click="dialogFormVisible = true">
+    创建新的团队
+  </el-button>
+  <el-dialog v-model="dialogFormVisible" title="欢迎来到寄了网站，请创建团队" center width="35%">
+    <el-form :model="form">
+      <el-form-item label="团队名称" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"/>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+  </el-dialog>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ArrowDown } from '@element-plus/icons-vue'
+import { reactive, ref } from 'vue'
 
-const handleClick = () => {
-  // eslint-disable-next-line no-alert
-  alert('button click')
-}
+const dialogTableVisible = ref(false)
+const dialogFormVisible = ref(false)
+const formLabelWidth = '140px'
+
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
 </script>
 <style scoped>
-.example-showcase .el-dropdown + .el-dropdown {
-  margin-left: 15px;
+.el-button--text {
+  margin-right: 15px;
 }
-.example-showcase .el-dropdown-link {
-  cursor: pointer;
-  color: var(--el-color-primary);
-  display: flex;
-  align-items: center;
+.el-select {
+  width: 300px;
 }
-
-.custom-icon-button {
-  background-image: url('@/assets/userAvtar.png'); /* 根据您的项目路径调整 */
-  background-size: 40px; /* 根据需要调整 */
-  background-repeat:no-repeat;
-  width: 40px; /* 根据需要调整 */
-  height: 40px; /* 根据需要调整 */
-
+.el-input {
+  width: 300px;
+}
+.dialog-footer button:first-child {
+  margin-right: 10px;
 }
 </style>

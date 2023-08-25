4a114236dac675a2d1be703f02136fa21dc97774
pesenteur <931373882@qq.com>
2023-08-25 10:25:58 +0800

    add Message

--- src/components/TeamNavi.vue
+++ src/components/TeamNavi.vue
@@ -8,7 +8,7 @@
   >
     <el-menu-item index="0">LOGO</el-menu-item>
     <div class="flex-grow" />
-    <el-menu-item index="1">Processing Center</el-menu-item>
+    <TeamMessage/>
     <el-sub-menu index="2">
       <template #title>个人中心</template>
       <TeamAvtar></TeamAvtar>
@@ -22,6 +22,7 @@
 <script lang="ts" setup>
 import { ref } from 'vue'
 import TeamAvtar from "./TeamAvtar.vue";
+import TeamMessage from "./TeamMessage.vue";
 
 const activeIndex = ref('1')
 const handleSelect = (key: string, keyPath: string[]) => {
